034533a2dc5721c9c9d1530f029ddbf120de5aed
Yalei Meng <yaleimeng@sina.com>
2017-10-17 14:32:19 +0800

    Crawler目录下的爬虫不是本代理池的必要组成部分。仅供学习之用

--- crawlers/get_All_66ip.py
+++ crawlers/get_All_66ip.py
@@ -1,20 +1,20 @@
 # -*- coding: utf-8 -*-
 '''
-@author: Yalei Meng    E-mail: yaleimeng@sina.com
-@license: (C) Copyright 2017, HUST Corporation Limited.
+@author: yaleimeng@sina.com
+@license: (C) Copyright 2017
 @DateTime: Created on 2017/10/9，at 9:16            '''
 from proxy import requestPage,write_file,verify_Proxies
 import re
 
 def get_All66():
       ip_exp = re.compile('\d+\..+\d+:\d+')     #采集国内的，高匿、超匿2种HTTP代理。
-      urls = ['http://www.66ip.cn/nmtq.php?getnum=500&isp=0&anonymoustype={}&start=&ports=' 
+      urls = ['http://www.66ip.cn/nmtq.php?getnum=300&isp=0&anonymoustype={}&start=&ports=' 
               '&export=&ipaddress=&area=1&proxytype=0&api=66ip'.format(str(i)) for i in range(3,5)]
       output = set()
       for url in urls:
           article = requestPage(url).__unicode__()
           out = ip_exp.findall(article)
-          output .update(out)          #把800个添加到集合里面
+          output .update(out)          #把找到的代理添加到集合里面
       print(output, '找到IP数量：', len(output))
       return output
 
